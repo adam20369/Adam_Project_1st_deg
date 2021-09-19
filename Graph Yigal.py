c67173527f78f9c86ffeaba5df6247e31372ab38
adam20369 <adamgit@bgu.ac.il>
2021-09-19 17:54:02 +0300

    3rd

---
+++ Graph Yigal.py
@@ -0,0 +1,93 @@
+import numpy as np
+from numpy.random import rand
+import matplotlib.pyplot as plt
+
+
+def initialstate(N):
+    state = 2 * np.random.randint(2, size=(N, N)) - 1
+    return state
+
+
+def mcmove(config, beta):
+    for i in range(N):
+        for j in range(N):
+            a = np.random.randint(0, N)
+            b = np.random.randint(0, N)
+            s = config[a, b]
+            nb = config[(a + 1) % N, b] + config[a, (b + 1) % N] + config[(a - 1) % N, b] + config[a, (b - 1) % N]
+            cost = 2 * s * nb
+            if cost < 0:
+                s *= -1
+            elif rand() < np.exp(-cost * beta):
+                s *= -1
+            config[a, b] = s
+    return config
+
+
+def calcEnergy(config):
+    energy = 0
+    for i in range(len(config)):
+        for j in range(len(config)):
+            S = config[i, j]
+            nb = config[(i + 1) % N, j] + config[i, (j + 1) % N] + config[(i - 1) % N, j] + config[i, (j - 1) % N]
+            energy += -nb * S
+    return energy / 4.
+
+
+def calcMag(config):
+    mag = np.sum(config)
+    return mag
+
+
+nt = 50  # number of temperature points
+N = 15  # size of the lattice, N x N
+eqSteps = 1024  # number of MC sweeps for equilibration
+mcSteps = 1024  # number of MC sweeps for calculation
+
+T = np.linspace(1.53, 3.28, nt);
+E, M, C, X = np.zeros(nt), np.zeros(nt), np.zeros(nt), np.zeros(nt)
+n1, n2 = 1.0 / (mcSteps * N * N), 1.0 / (mcSteps * mcSteps * N * N)
+for tt in range(nt):
+    E1 = M1 = E2 = M2 = 0
+    config = initialstate(N)
+    iT = 1.0 / T[tt]
+    iT2 = iT * iT
+
+    for i in range(eqSteps):  # equilibrate
+        mcmove(config, iT)  # Monte Carlo moves
+
+    for i in range(mcSteps):
+        mcmove(config, iT)
+        Ene = calcEnergy(config)  # calculate the energy
+        Mag = calcMag(config)  # calculate the magnetisation
+
+        E1 = E1 + Ene
+        M1 = M1 + Mag
+        M2 = M2 + Mag * Mag
+        E2 = E2 + Ene * Ene
+
+    E[tt] = n1 * E1
+    M[tt] = n1 * M1
+    C[tt] = (n1 * E2 - n2 * E1 * E1) * iT2
+    X[tt] = (n1 * M2 - n2 * M1 * M1) * iT
+f = plt.figure(figsize=(18, 10))
+
+
+f.add_subplot(2, 2, 1)
+plt.scatter(T, abs(M), s=50, marker='o', color='RoyalBlue')
+plt.xlabel("Temperature (T)", fontsize=20)
+plt.ylabel("Magnetization ", fontsize=20)
+plt.axis('tight')
+
+
+f.add_subplot(2, 2, 2)
+plt.scatter(T, X, s=50, marker='o', color='RoyalBlue')
+plt.xlabel("Temperature (T)", fontsize=20)
+plt.ylabel("Susceptibility", fontsize=20)
+plt.axis('tight')
+
+f.add_subplot(2, 2, 3)
+plt.scatter(np.log(T), np.log(X), s=50, marker='o', color='RoyalBlue')
+plt.xlabel("Log Temperature (T)", fontsize=20)
+plt.ylabel("Log Susceptibility", fontsize=20)
+plt.axis('tight')
