6f254defce5b5cbe6ad1e529a7a4819a8d793858
Adam <adamgit@post.bgu.ac.il>
2021-10-30 23:02:31 +0300

    Run_R_metric_impure

--- Run_R_metric.py
+++ Run_R_metric.py
@@ -10,10 +10,10 @@ from PXP_TI_Bath import *
 def plotRmetric(n_TI, theta_i, theta_f, res):
     theta=np.linspace(theta_i, theta_f, res)
     for t in np.nditer(theta):
-        r=RunRmetric(n_TI, np.sin(t), np.cos(t), TIOBCNew)
+        r=RunRmetric(n_TI, np.sin(t), np.cos(t), TIOBCNewImpure) #IMPURE MODEL!!
         plt.plot(t, r,  marker= '.', color='C4')
-    plt.title('11 Atoms')
+    plt.title('Angle dependance of r-metric, {} Atoms'.format(n_TI))
     plt.xlabel(r"$\theta$")
     plt.ylabel(r"$\langle(r)\rangle$")
-
+# plotRmetric(10, 1, 2, 500)
 # plt.show()
--- Run_Time_Proposal.py
+++ Run_Time_Proposal.py
@@ -10,13 +10,13 @@ from PXP_TI_Bath import *
 def RunTimeProp4(n_tot, n_Array, Coupl=Z_i, h_x=1, h_z=1, T_max=20): #time propagation of 4 different PXP atom sizes (total N conserved)
     markers = np.array(('s', '^', 'o', 'd'))
     colors = np.array(('b','r','y','k'))
-    n_start= n_Array[0]
+    n_start = n_Array[0]
     for n in n_Array:
         H = PXPBathHam(n_tot, n, Coupl, h_x, h_z)
         EV = EvecEval(H)
         Neel = Neelstate(n_tot)
         Color = colors[n-n_start]
-        marker= markers[n-n_start]
+        marker = markers[n-n_start]
         TimeProp(EV, n_tot, Neel, T_max, Color, marker)
     custom_lines = [Line2D([0], [0], color=colors[0], marker=markers[0]),
                     Line2D([0], [0], color=colors[1], marker=markers[1]),
