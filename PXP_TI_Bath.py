94cbe128dac9fcbf6aa8302549a9b27204b96549
Adam <adamgit@post.bgu.ac.il>
2021-10-30 20:27:55 +0300

    run_time_proposal

--- PXP_TI_Bath.py
+++ PXP_TI_Bath.py
@@ -388,7 +388,7 @@ def TimePropPXP(EigenEnVecs, Subspcdim, Spans,
 #TimePropPXP(EvecEval(SubspaceMat(m, Hamiltonian)),Subspccount(m,Hamiltonian), EigenSpan(EvecEval(SubspaceMat(m, Hamiltonian)), SubspcNeelstate(m)),T_max)
 
 def TimeProp(EigenEnVecs, n_tot, Nstate,
-             T_max, Color,marker):  # N_TotGENERAL DIM  time propagation of each eigenstate with it's corresponding eigenenergy
+             T_max, Color, marker):  # N_TotGENERAL DIM  time propagation of each eigenstate with it's corresponding eigenenergy
     Eval, Evec = EigenEnVecs
     w = np.dot(1 / (np.sqrt(normconst(EigenSpan(EigenEnVecs, Nstate)))), (EigenSpan(EigenEnVecs, Nstate)))
     t = np.arange(1, T_max, 0.05)
@@ -401,21 +401,10 @@ def TimeProp(EigenEnVecs, n_tot, Nstate,
             Z_2t = Z_2t + np.dot(np.dot((np.exp(-1j * Eval[j] * t)), w[j]),
                                  Evec[:, j])  # Z_2(t) spanned in eigenstate basis as Cols of a matrix
         y = (np.absolute(np.dot(np.conjugate((Z_2)), (Z_2t)))) ** 2
-        plt.plot(t, np.round(y, 4), marker=marker, markersize=3, color=Color)
+        plt.plot(t, np.round(y, 4), marker=marker, markersize=2, color=Color)
     #plt.show()
 #TODO FIX NORMALIZATION!!!
 
-
-def RunTimeProp(n_tot, n, Coupl=Z_i, h_x=1, h_z=1, T_max=20):#only even numbers right now
-    """
-    Runs TimeProp
-    """
-    H = PXPBathHam(n_tot, n, Coupl, h_x, h_z)
-    # H= PXPBathHamUncoup(n_tot, n, Coupl, h_x, h_z) # Uncoupled version
-    EV = EvecEval(H)
-    Neel = Neelstate(n_tot)
-    Color = np.array((np.random.rand(), np.random.rand(), np.random.rand()))
-    TimeProp(EV, n_tot, Neel, T_max,Color)
 
 
 #TODO check if you can throw
--- Run_Time_Proposal.py
+++ Run_Time_Proposal.py
@@ -4,19 +4,38 @@ import matplotlib.pyplot as plt
 import itertools
 import timeit
 from scipy import integrate
+from matplotlib.lines import Line2D
 from PXP_TI_Bath import *
+
+def RunTimeProp(n_tot, n, Coupl=Z_i, h_x=1, h_z=1, T_max=20):# Time propagation of PXP TI COUPLED
+    """
+    Runs TimeProp
+    """
+    H = PXPBathHam(n_tot, n, Coupl, h_x, h_z)
+    # H= PXPBathHamUncoup(n_tot, n, Coupl, h_x, h_z) # Uncoupled version
+    EV = EvecEval(H)
+    Neel = Neelstate(n_tot)
+    Color = np.array((np.random.rand(), np.random.rand(), np.random.rand()))
+    markers = np.random.choice(np.array(('s', '^', 'o', '*'))) #TODO could be broken
+    TimeProp(EV, n_tot, Neel, T_max, Color, markers)
 
 
 def RunTimeProp4(n_tot, n_Array, Coupl=Z_i, h_x=1, h_z=1, T_max=20): #time propagation of 4 different PXP atom sizes (total N conserved)
-    markers = itertools.cycle(('s', '^', 'X', 'o', '*', 'h', 'D'))
+    markers = np.array(('s', '^', 'o', '*'))
+    colors = np.array(('b','r','y','k'))
+    n_start= n_Array[0]
     for n in n_Array:
         H = PXPBathHam(n_tot, n, Coupl, h_x, h_z)
         EV = EvecEval(H)
         Neel = Neelstate(n_tot)
-        Color = np.array((np.random.rand(), np.random.rand(), np.random.rand()))
-        marker= next(markers)
+        Color = colors[n-n_start]
+        marker= markers[n-n_start]
         TimeProp(EV, n_tot, Neel, T_max, Color, marker)
-    plt.legend(['{} atoms'.format(n_Array[0]),'{} atoms'.format(n_Array[1]),'{} atoms'.format(n_Array[2]),'{} atoms'.format(n_Array[3])])
+    custom_lines = [Line2D([0], [0], color=colors[0], marker=markers[0]),
+                    Line2D([0], [0], color=colors[1], marker=markers[1]),
+                    Line2D([0], [0], color=colors[2], marker=markers[2]),
+                    Line2D([0], [0], color=colors[3], marker=markers[3])] #LEGEND DEFINITIONS
+    plt.legend(custom_lines, ['{} atoms'.format(n_Array[0]), '{} atoms'.format(n_Array[1]), '{} atoms'.format(n_Array[2]),'{} atoms'.format(n_Array[3])])
     plt.xlabel('$t$')
     plt.ylabel(r'$|\langle\mathbb{Z}_{2}|\mathbb{Z}_{2}(t)\rangle|^{2}$')
     plt.savefig('new fidelity_12atoms.pdf')
