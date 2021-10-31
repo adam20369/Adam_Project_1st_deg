d277779f2a6ca55d6d0f00bf5991e79084579543
Adam <adamgit@post.bgu.ac.il>
2021-10-31 14:20:21 +0200

    Plot_ETH_script_update

--- PXP_TI_Bath.py
+++ PXP_TI_Bath.py
@@ -322,33 +322,7 @@ def Fig2A(EigenEnVecs,
 # Fig2A(EvecEval(SubspaceMat(m, Hamiltonian)),SubspaceMat(m, O_z(m))) #TODO the O_z is good
 # Fig2A(EvecEval(SubspaceMat(m, Hamiltonian)),SubspaceMat(m, Z_1(m)))
 
-def plotFig2A(m,n,p,Hamiltonian,Op): #plotting for 3 atom sizes
-    Eval1, Evec1 = EvecEval(SubspaceMat(m,Hamiltonian(m)))
-    Eval2, Evec2 = EvecEval(SubspaceMat(n,Hamiltonian(n)))
-    Eval3, Evec3 = EvecEval(SubspaceMat(p,Hamiltonian(p)))
-    x = np.matmul(np.transpose(np.conjugate(Evec1)), np.matmul(SubspaceMat(m,Op(m)), Evec1))
-    y = np.matmul(np.transpose(np.conjugate(Evec2)), np.matmul(SubspaceMat(n,Op(n)), Evec2))
-    z = np.matmul(np.transpose(np.conjugate(Evec3)), np.matmul(SubspaceMat(p,Op(p)), Evec3))
-    fig, (ax1, ax2, ax3)=  plt.subplots(3)
-    for j in range(0, np.size(Eval1)):
-        # print("\n <Operator(", Eval[j], ")>:", np.real(np.round(y[j,j],3)))
-        ax1.plot(Eval1[j], np.real(x[j, j]), marker='.', color='C2')
-        ax1.set_title('8 Atoms')
-        ax1.set(xlabel='E', ylabel=r'$\langle n|\hat{O}^{Z}|n\rangle$')
-    for i in range(0, np.size(Eval2)):
-        # print("\n <Operator(", Eval[j], ")>:", np.real(np.round(y[j,j],3)))
-        ax2.plot(Eval2[i], np.real(y[i, i]), marker='.', color='C3')
-        ax2.set_title('10 Atoms')
-        ax2.set(xlabel='E', ylabel=r'$\langle n|\hat{O}^{Z}|n\rangle$')
-    for k in range(0, np.size(Eval3)):
-        ax3.plot(Eval3[k], np.real(z[k, k]), marker='.', color='C4')
-        ax3.set_title('12 Atoms')
-        ax3.set(xlabel='E', ylabel=r'$\langle n|\hat{O}^{Z}|n\rangle$')
-    plt.savefig('fig1new')
-    plt.show()
 
-
-# plotFig2A(8,10,12,PXPOBCNew,O_z) #TODO the O_z is good
 
 def Fig3B(EigenEnVecs, Nstate):  # ONLY EVEN NUM OF ATOMS
     Eval, Evec = EigenEnVecs
---
+++ Plot_ETH_3Sizes.py
@@ -0,0 +1,35 @@
+import numpy as np
+import numpy.linalg as la
+import matplotlib.pyplot as plt
+import itertools
+import timeit
+from scipy import integrate
+from PXP_TI_Bath import *
+
+def plotFig2A(m,n,p,Hamiltonian,Op): #plotting for 3 atom sizes
+    Eval1, Evec1 = EvecEval(SubspaceMat(m,Hamiltonian(m)))
+    Eval2, Evec2 = EvecEval(SubspaceMat(n,Hamiltonian(n)))
+    Eval3, Evec3 = EvecEval(SubspaceMat(p,Hamiltonian(p)))
+    x = np.matmul(np.transpose(np.conjugate(Evec1)), np.matmul(SubspaceMat(m,Op(m)), Evec1))
+    y = np.matmul(np.transpose(np.conjugate(Evec2)), np.matmul(SubspaceMat(n,Op(n)), Evec2))
+    z = np.matmul(np.transpose(np.conjugate(Evec3)), np.matmul(SubspaceMat(p,Op(p)), Evec3))
+    fig, (ax1, ax2, ax3)=  plt.subplots(3, sharex='all')
+    for j in range(0, np.size(Eval1)):
+        # print("\n <Operator(", Eval[j], ")>:", np.real(np.round(y[j,j],3)))
+        ax1.plot(Eval1[j], np.real(x[j, j]), marker='.', color='C2')
+        ax1.legend(['8 Atoms'],loc=1)
+        ax1.set_title('ETH Conjecture of {}'.format(r'$\hat{O}^{Z}$'))
+        ax1.set(ylabel=r'$\langle n|\hat{O}^{Z}|n\rangle$')
+    for i in range(0, np.size(Eval2)):
+        # print("\n <Operator(", Eval[j], ")>:", np.real(np.round(y[j,j],3)))
+        ax2.plot(Eval2[i], np.real(y[i, i]), marker='.', color='C3')
+        ax2.legend(['10 Atoms'],loc=1)
+        ax2.set(ylabel=r'$\langle n|\hat{O}^{Z}|n\rangle$')
+    for k in range(0, np.size(Eval3)):
+        ax3.plot(Eval3[k], np.real(z[k, k]), marker='.', color='C4')
+        ax3.legend(['12 Atoms'], loc=1)
+        ax3.set(xlabel='E', ylabel=r'$\langle n|\hat{O}^{Z}|n\rangle$')
+    #plt.savefig('fig1new')
+    #plt.show()
+
+# plotFig2A(8,10,12,PXPOBCNew,O_z)
