525a3417ac5df83cafac041c90e778bc185de7e7
Adam <adamgit@post.bgu.ac.il>
2023-02-22 23:10:45 +0200

    Bash files fix for Sleep for queue bigger than 1490 jobs!

--- FFT_script_realizations_gammas_h_c.sh
+++ FFT_script_realizations_gammas_h_c.sh
@@ -4,11 +4,19 @@ n_PXP='10'
 n_TI_max='13'
 h_c_max='2'
 Sample_no='1000'
+Sleep='20'
 sd='5' #doesn't matter here
 for n_TI in $(seq $n_PXP 1 $n_TI_max)
   do
     for h_c in $(seq 0 0.1 $h_c_max)
       do
+        r=$(qstat|grep adamgit|wc -l)
+        while [ $r -ge 1490 ]
+          do
+            echo sleep $Sleep seconds load is $r
+            sleep $Sleep
+            r =$(qstat|grep adamgit|wc -l)
+          done
         qsub -q intel_all.q -N PXP_$n_TI FFT_py_script.sh $n_PXP $n_TI $h_c $sd $Sample_no $seed_max;
       done
   done;
